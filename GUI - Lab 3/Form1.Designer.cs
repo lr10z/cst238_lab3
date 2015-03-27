356f535626f50b0fb4668bb3f0d1e766a9534d68
Leander Rodriguez <lr10@me.com>
2015-03-27 16:48:42 -0700

    Initial commit

---
+++ GUI - Lab 3/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace GUI___Lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nextFlag = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nextFlag
+            // 
+            this.nextFlag.Location = new System.Drawing.Point(0, 285);
+            this.nextFlag.Name = "nextFlag";
+            this.nextFlag.Size = new System.Drawing.Size(284, 23);
+            this.nextFlag.TabIndex = 3;
+            this.nextFlag.Text = "Next Flag";
+            this.nextFlag.UseVisualStyleBackColor = true;
+            this.nextFlag.Click += new System.EventHandler(this.nextFlag_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 165);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 165);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 36);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 36);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.selection);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(94, 0);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 36);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.selection);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Location = new System.Drawing.Point(188, 0);
+            this.button3.Margin = new System.Windows.Forms.Padding(0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 36);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.selection);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 234);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(284, 48);
+            this.tableLayoutPanel2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 48);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(145, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 48);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.label4, 1, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 201);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(284, 33);
+            this.tableLayoutPanel3.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 33);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Status";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(145, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 33);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Score";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 308);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.nextFlag);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button nextFlag;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ GUI - Lab 3/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI___Lab_3
+{
+    public partial class Form1 : Form
+    {
+        private Image[] m_flags;
+        private string[] m_answers;
+        private string[] m_countries;
+        private int m_currentIndex;
+        private int m_numFlags;
+        private int m_numButtons;
+        private int m_score;
+        private int m_minValue;
+        static Random m_rand;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            m_flags = new Image[10] {   Properties.Resources.Australia, Properties.Resources.Brazil, 
+                                        Properties.Resources.Canada, Properties.Resources.Germany,
+                                        Properties.Resources.India, Properties.Resources.Japan,
+                                        Properties.Resources.Netherlands, Properties.Resources.Singapore,
+                                        Properties.Resources.Thailand, Properties.Resources.UnitedStates};
+
+            m_answers = new string[10] {  "Australia", "Brazil", "Canada", "Germany", "India", "Japan", 
+                                          "Netherlands", "Singapore", "Thailand", "United States"};
+
+            m_countries = new string[10] {  "Belgium", "Egypt", "Ethiopia", "Lithuania", "Philippines", 
+                                            "New Zealand", "Nigeria", "Russia", "Portugal", "Wales"};
+
+            m_currentIndex = 0;
+            m_numFlags = 10;
+            m_numButtons = 3;
+            m_score = 0;
+            m_minValue = 0;
+            m_rand = new Random();
+        }
+
+        private void nextFlag_Click(object sender, EventArgs e)
+        {
+            int randNum;
+
+            // answer feedback is cleared
+            label1.Text = "";
+
+            // generates a random flag and displays it
+            randNum = m_rand.Next(m_minValue, m_numFlags);
+            m_currentIndex = randNum;
+            pictureBox1.Image = m_flags[m_currentIndex];
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            // generates random buttons to display, one of which is the correct answer
+            randNum = m_rand.Next(m_minValue, m_numButtons);
+
+            switch(randNum)
+            {
+                case 1: 
+                    button2.Text = m_answers[m_currentIndex];
+                    button3.Text = m_countries[m_rand.Next(m_minValue, m_numFlags)];
+                    button1.Text = m_countries[m_rand.Next(m_minValue, m_numFlags)];
+                    break;
+
+                case 2: 
+                    button3.Text = m_answers[m_currentIndex];
+                    button1.Text = m_countries[m_rand.Next(m_minValue, m_numFlags)];
+                    button2.Text = m_countries[m_rand.Next(m_minValue, m_numFlags)];
+                    break;
+
+                default :
+                    button1.Text = m_answers[m_currentIndex];
+                    button2.Text = m_countries[m_rand.Next(m_minValue, m_numFlags)];
+                    button3.Text = m_countries[m_rand.Next(m_minValue, m_numFlags)];
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int randNum;
+
+            // generates a random flag and displays it
+            randNum = m_rand.Next(m_minValue, m_numFlags);
+            m_currentIndex = randNum;
+            pictureBox1.Image = m_flags[m_currentIndex];
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            // generates initial buttons to display, one of which is the correct answer
+            button1.Text = m_countries[1];
+            button2.Text = m_answers[m_currentIndex];
+            button3.Text = m_countries[2];
+        }
+
+        private void selection(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            // checks if the button clicked matches flag displayed
+            if(btn.Text == m_answers[m_currentIndex])
+            {
+                label1.Text = "Correct!";
+                ++m_score;
+                label2.Text = m_score.ToString();
+            }
+            else
+            {
+                label1.Text = "Oops! Try again.";
+            }
+        }
+    }
+}
